966e9e677715235d18d56c664af3a14cd6ab7c3d
AtaMakesStuff <sinankit@udel.edu>
2024-12-05 08:44:35 -0500

    did health and lives

--- FinalProject3D/Assets/Scripts/Collisions.cs
+++ FinalProject3D/Assets/Scripts/Collisions.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class Collisions : MonoBehaviour
 {
     public int playerHealth = 100;
+
+    public int healthChange = 10;
+
+    public string newHealthPointsText;
+
+    public TextMeshProUGUI healthDisplay;
+
+    public int playerLives = 3;
+
+    public Vector3 start = (0.0f,0.5f,0.0f);
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerHealth < 10)
+        {
+            playerLives = playerLives -1;
+            playerHealth = 100;
+            transform.position = start;
+        }
         
     }
 
@@ -22,6 +41,9 @@ public class Collisions : MonoBehaviour
         if (collision.gameObject.CompareTag("Hazard"))
         {
             playerHealth = playerHealth - 10;
+            newHealthPointsText = "" + playerHealth;
+            healthDisplay.text = newHealthPointsText;
         }
     }
+
 }
--- FinalProject3D/Assets/Scripts/Hazards.cs
+++ FinalProject3D/Assets/Scripts/Hazards.cs
@@ -6,8 +6,6 @@ using UnityEngine.SceneManagement;
 public class Hazards : MonoBehaviour
 {
 
-    public int playerHealth = 100;
-
     // Start is called before the first frame update
     void Start()
     {
