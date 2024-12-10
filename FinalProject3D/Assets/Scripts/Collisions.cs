08a344952b876d89f6105f992cf1d5dafa6ea73e
AtaMakesStuff <sinankit@udel.edu>
2024-12-10 09:19:33 -0500

    made game over a splash screen

--- FinalProject3D/Assets/Scripts/Collisions.cs
+++ FinalProject3D/Assets/Scripts/Collisions.cs
@@ -23,12 +23,35 @@ public class Collisions : MonoBehaviour
 
     public TextMeshProUGUI livesDisplay;
 
-    public static int previousSceneNumber;
+    public GameObject purple; 
+
+    public GameObject gameOver; 
+
+    public GameObject titleScreenButton;
+
+    public GameObject titleScreenText; 
+
+    public GameObject retryButton;
+
+    public GameObject retryText; 
+
+    public GameObject healthText;
+
+    public GameObject healthNumbers;
+
+    public GameObject livesText;
+
+    public GameObject livesNumbers;
 
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
+        purple.SetActive(false);
+        gameOver.SetActive(false);
+        titleScreenButton.SetActive(false);
+        titleScreenText.SetActive(false);
+        retryButton.SetActive(false);
+        retryText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -62,14 +85,17 @@ public class Collisions : MonoBehaviour
 
         if (playerLives < 1)
         {
-            previousSceneNumber = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(3);
+            purple.SetActive(true);
+            gameOver.SetActive(true);
+            titleScreenButton.SetActive(true);
+            titleScreenText.SetActive(true);
+            retryButton.SetActive(true);
+            retryText.SetActive(true);
+            healthText.SetActive(false);
+            healthNumbers.SetActive(false);
+            livesText.SetActive(false);
+            livesNumbers.SetActive(false);
         }
-    }
-
-    public void loadPreviousScene()
-    {
-        SceneManager.LoadScene(previousSceneNumber);
     }
 
 }
